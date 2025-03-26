8ea76392ff49f454f92b944e0e540b0ffddd8dec
kanayabdielaa <kanayabdielaa@gmail.com>
2025-03-26 12:30:44 +0800

    js5 Praktikum 2

--- Week5.2/routes/web.php
+++ Week5.2/routes/web.php
@@ -28,3 +28,4 @@ Route::get('/user/tambah', [UserController::class, 'tambah']);
 Route::get('/user', [UserController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/', [WelcomeController::class, 'index']);
